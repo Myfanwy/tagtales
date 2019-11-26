45c3514b0f89c91f56c5b7bb186dbfb9f9bb2f9e
Myfanwy <mrowlan1@gmail.com>
2019-11-26 11:33:06 -0800

    small additions/removals

--- R/bard_query.R
+++ R/bard_query.R
@@ -6,6 +6,7 @@
 #' @param baseurl BARD query homepage
 #' @param curlH curl handle; defaults to curl, which if you've run setup.R is in the global environment; running the following code will recreate it: \code{curl = getCurlHandle(useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36", followlocation = TRUE)}
 #' @return a data frame of BARD detections; note that these detections are in a summarized format.
+#' @author Matthew Espe
 #' @export
 #' @examples
 #' bard_query(tagIDS = bard_tags(c(2841, 2842)), curlH = getCurlHandle(useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36", followlocation = TRUE) )
